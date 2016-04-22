297634b74707995ad215572aa3bd70ec7d4de7f5
Simon Temple <simonctemple@live.com>
2016-04-22 10:27:14 -0700

    add edit functionality for posts

--- src/MegansBlog/Contollers/PostsController.cs
+++ src/MegansBlog/Contollers/PostsController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Mvc;
 using MegansBlog.Models;
+using Microsoft.Data.Entity;
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -25,7 +26,7 @@ namespace MegansBlog.Contollers
             return View(thisPost);
         }
 
-        //Create new Post
+        // Create new Post
         public IActionResult Create()
         {
             return View();
@@ -35,6 +36,21 @@ namespace MegansBlog.Contollers
         public IActionResult Create(Post post)
         {
             db.Posts.Add(post);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        // Edit Post
+        public IActionResult Edit(int id)
+        {
+            var thisPost = db.Posts.FirstOrDefault(posts => posts.PostId == id);
+            return View(thisPost);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Post post)
+        {
+            db.Entry(post).State = EntityState.Modified;
             db.SaveChanges();
             return RedirectToAction("Index");
         }
