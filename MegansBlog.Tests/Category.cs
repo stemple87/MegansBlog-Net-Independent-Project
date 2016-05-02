4f3b3bfb3cdb9e491dc011967f906fdafce6975c
simon <simonctemple@live.com>
2016-05-01 18:11:20 -0700

    add working tests

---
+++ MegansBlog.Tests/Category.cs
@@ -0,0 +1,23 @@
+﻿using MegansBlog.Models;
+using System;
+using Xunit;
+
+namespace MegansBlog.Tests
+{
+    public class CategoryTest
+    {
+
+        [Fact]
+        public void GetCategoryTest()
+        {
+            //Arrange
+            var category = new Category();
+
+            //Act
+            category.Name = "Name";
+
+            //Assert
+            Assert.Equal("Name", category.Name);
+        }
+    }
+}
---
+++ MegansBlog.Tests/Post.cs
@@ -0,0 +1,29 @@
+﻿using MegansBlog.Models;
+using System;
+using Xunit;
+
+namespace MegansBlog.Tests
+{
+    public class PostTest
+    {
+        [Fact]
+        public void GetPostTest()
+        {
+            //Arrange
+            var post = new Post();
+            var currentTime = DateTime.Now;
+
+
+            //Act
+            post.Title = "Title";
+            post.Body = "Body";
+            post.PostDate = currentTime;
+
+
+            //Assert
+            Assert.Equal("Title", post.Title);
+            Assert.Equal("Body", post.Body);
+            Assert.Equal(currentTime, post.PostDate);
+        }
+    }
+}
